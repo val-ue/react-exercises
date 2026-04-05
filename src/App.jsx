0d133aa64e046aa58ad36587fe3911e99d894203
val_l <contacttovalerie@gmail.com>
2026-04-05 12:27:42 -0400

    Added isOpen for outerlist

--- src/App.jsx
+++ src/App.jsx
@@ -2,157 +2,57 @@ import React from "react";
 import "./styles.css";
 
 const Item = (props) => (
-  <li
-    data-testid="todo-item"
-    className={props.item.completed ? "item-completed" : ""}
-  >
-    {props.item.value}
-    <button
-      data-testid="toggle-button"
-      onClick={() => props.handleToggle(props.item)}
-    >
-      Toggle
-    </button>
-    <button
-      data-testid="delete-button"
-      onClick={() => props.handleRemove(props.item)}
-    >
-      Remove
-    </button>
-    <input
-      type="number"
-      onChange={(e) => props.handlePriority(props.item, e.target.value)}
-      id="priority"
-      name="priority"
-      value={props.item.priority || 1}
-      min="1"
-      max="5"
-    />
+  <li onClick={() => props.handleClick(props.listItem)}>
+    {props.description}
+
+    {props.listItem.isOpen && (
+      <ul>
+        {props.innerList.map((subListItem) => {
+          return <li>{subListItem}</li>;
+        })}
+      </ul>
+    )}
   </li>
 );
 
-const List = (props) => (
-  <ul data-testid="todo-list">
-    {props.list.map((item) => (
-      <Item
-        key={item.id}
-        item={item}
-        handleToggle={props.handleToggle}
-        handleRemove={props.handleRemove}
-        handlePriority={props.handlePriority}
-      />
-    ))}
-  </ul>
-);
-
-class Form extends React.Component {
-  state = {
-    inputValue: "",
-  };
-  handleChange = (e) => {
-    this.setState({ inputValue: e.target.value });
-  };
-
-  handleSubmit = (e) => {
-    console.log("submitted");
-    e.preventDefault();
-    const value = this.state.inputValue;
-
-    this.setState({ inputValue: "" });
-    this.props.handleSubmit(value);
-  };
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <input
-          data-testid="add-todo"
-          onChange={this.handleChange}
-          value={this.state.inputValue}
-        />
-      </form>
-    );
-  }
-}
-
 class App extends React.Component {
   state = {
-    list: [],
-    filterValue: "",
+    outerList: [
+      { name: "option1", isOpen: false },
+      { name: "option2", isOpen: false },
+      { name: "option3", isOpen: false },
+    ],
+    innerList: ["apple", "mango", "orange"],
   };
 
-  handlePriority = (item, value) => {
-    const newList = this.state.list.map((element) => {
-      if (element.id === item.id) {
-        return { ...element, priority: Number(value) };
+  handleClick = (listItem) => {
+    const newListItem = { ...listItem };
+    newListItem.isOpen = !listItem.isOpen;
+
+    const newOuterList = this.state.outerList.map((element) => {
+      if (element === listItem) {
+        return newListItem;
       }
       return element;
     });
-    this.setState({ list: newList });
-  };
-
-  handleSort = () => {
-    const newList = [...this.state.list];
-    newList.sort((a, b) => (b.priority || 0) - (a.priority || 0));
-    this.setState({ list: newList });
-  };
-
-  handleFilterUpdate = (value) => {
-    this.setState({ filterValue: value });
-  };
-
-  handleSubmit = (value) => {
-    const item = {
-      value,
-      completed: false,
-      id: `${Math.random()}-${Math.random()}`,
-    };
-    const newList = [...this.state.list, item];
-    this.setState({ list: newList });
-  };
-
-  handleToggle = (item) => {
-    const newList = this.state.list.map((element) => {
-      if (element.id === item.id) {
-        element.completed = !element.completed;
-      }
-      return element;
-    });
-    this.setState({ list: newList });
-  };
-
-  handleRemove = (item) => {
-    const removeItem = this.state.list.filter(
-      (element) => element.id !== item.id
-    );
-    this.setState({ list: removeItem });
+    this.setState({ outerList: newOuterList });
   };
 
   render() {
-    const filteredList = this.state.list.filter((element) => {
-      return element.value.includes(this.state.filterValue);
-    });
-
     return (
-      <div className="App">
-        <Form
-          handleSubmit={this.handleSubmit}
-          handleFilterUpdate={this.handleFilterUpdate}
-        />
-
-        <input
-          onChange={(e) => this.handleFilterUpdate(e.target.value)}
-          value={this.state.filterValue}
-        />
-
-        <List
-          list={filteredList}
-          handleToggle={this.handleToggle}
-          handleRemove={this.handleRemove}
-          handlePriority={this.handlePriority}
-        />
-
-        <button onClick={this.handleSort}>Sort</button>
-      </div>
+      <ol>
+        {this.state.outerList.map((listItem, index) => {
+          return (
+            <Item
+              key={index}
+              handleClick={this.handleClick}
+              description={listItem.name}
+              listItem={listItem}
+              innerList={this.state.innerList}
+            />
+          );
+        })}
+      </ol>
     );
   }
 }
